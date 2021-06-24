6f23f3de8aa8e6f031c8938d469dfe4d965dbe84
Platin Iradukunda <80734313+platiniradukunda@users.noreply.github.com>
2021-06-24 11:38:40 -0400

    link fixed

--- React Notes App/notes_app2/src/components/Thanks.jsx
+++ React Notes App/notes_app2/src/components/Thanks.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
-function Thanks(props) {
+function Thanks() {
     return (
-        <div>
+        <div className="thanks">
             <h3>Thanks for review!!</h3>
             <Link to="/"><button>Back to Notes</button></Link>
         </div>
