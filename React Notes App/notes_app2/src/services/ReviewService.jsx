ccb3dc95f4c1163ff312bf85e7b5b434bc08e382
Platin Iradukunda <80734313+platiniradukunda@users.noreply.github.com>
2021-06-24 11:47:20 -0400

    edit in review service

--- React Notes App/notes_app2/src/services/ReviewService.jsx
+++ React Notes App/notes_app2/src/services/ReviewService.jsx
@@ -5,8 +5,8 @@ const review_url = "http://localhost:8080/review";
 class ReviewService {
 
     // method to post review to DB
-    postReview(itemsInState) {
-        return axios.post(review_url, itemsInState)
+    postReview(reviewInState) {
+        return axios.post(review_url, reviewInState)
     }
 
 }
