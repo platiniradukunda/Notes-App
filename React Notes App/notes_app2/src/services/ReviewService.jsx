c1447e3102255bf49d92de26cfc5aa4859d269af
Platin Iradukunda <80734313+platiniradukunda@users.noreply.github.com>
2021-06-23 23:29:45 -0400

    created service for second model

---
+++ React Notes App/notes_app2/src/services/ReviewService.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const review_url = "http://localhost:8080/review";
+
+class ReviewService {
+
+    // method to post review to DB
+    postNotes(itemsInState) {
+        return axios.post(review_url, itemsInState)
+    }
+
+}
+
+export default new ReviewService();
