ca7b173fbd49ea3ae529048a74522d9a3a3c3d41
Platin Iradukunda <80734313+platiniradukunda@users.noreply.github.com>
2021-06-24 15:18:46 -0400

    Adding css

--- React Notes App/notes_app2/src/components/ReviewPostPage.jsx
+++ React Notes App/notes_app2/src/components/ReviewPostPage.jsx
@@ -19,7 +19,6 @@ class ReviewPostPage extends Component {
     // function to post the review to the database
     addReview = () => {
         ReviewService.postReview(this.state).then(response=>{console.log(response)}).catch(error=>{console.log(error)});
-        // window.location.reload(false);
     }
 
     render() {
@@ -28,14 +27,15 @@ class ReviewPostPage extends Component {
                 <div className="back">
                     {/* link to get back to the notes page to add/edit notes */}
                     <Link to="/"><button>Notes</button></Link>
-                </div>
-                    {/* input and textarea to enter name and review */}
-                <div className="reviewEdit">
+                    <div className="reviewEdit">
                     <input type="text" placeholder="Enter name here..." onChange={(e)=>this.handleChange(e)} id="name"/>
                     <textarea name="" onChange={(e)=>this.handleChange(e)} id="review" placeholder="Enter review here..."></textarea>
                     {/* calling the function that adds to database when clicked and also links to a different component that shows thanks for review message */}
                     <Link to="/thanks"><button onClick={this.addReview}>Submit</button></Link>
                 </div>
+                </div>
+                    {/* input and textarea to enter name and review */}
+                
                 
             </div>
         );
