d71504366e2d8cb8aa699bb24d9c32a67a470871
Platin Iradukunda <80734313+platiniradukunda@users.noreply.github.com>
2021-06-24 23:12:42 -0400

    comments

--- React Notes App/notes_app2/src/components/Thanks.jsx
+++ React Notes App/notes_app2/src/components/Thanks.jsx
@@ -5,6 +5,7 @@ function Thanks() {
     return (
         <div className="thanks">
             <h3>Thanks for review!!</h3>
+            {/* link to go back to notes ie NavBar component that has the notes */}
             <Link to="/"><button>Back to Notes</button></Link>
         </div>
     );
